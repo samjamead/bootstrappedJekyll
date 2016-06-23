11f1e2a67403dcd86f6cfd2403bbf455c1e9193a
Sam Mead <sam@mead.fm>
2016-06-23 09:45:09 +0800

    Don't open links in new tabs

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,6 +3,10 @@
 $("a[href^='http://']").attr("target","_blank");
 $("a[href^='https://']").attr("target","_blank");
 
+// Unless it's because of Github pages
+$("a[href^='http://mead.fm/bootstrappedJekyll/']").attr("target","_self");
+$("a[href^='https://mead.fm/bootstrappedJekyll/']").attr("target","_self");
+
 // Dynamically set bottom margin of body for sticky footer
 function stickyFooter() {
   var bodyBottomMargin = $( '.site-footer' ).outerHeight();
